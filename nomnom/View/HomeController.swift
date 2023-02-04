6f001abe1a6c1ecc1257ecb5dcf0b11dfb062a57
fredycamas <fcamas62@gmail.com>
2023-02-03 23:03:46 -0500

    add colection view

--- nomnom/View/HomeController.swift
+++ nomnom/View/HomeController.swift
@@ -7,21 +7,77 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController{
+import UIKit
+struct CompositionalLayout {
     
-    var searchBtnPressed = false
+    func makeCompositionalLayoutViewControllerWithZoomingCarousel() -> UICollectionViewCompositionalLayout {
+        let compositionalLayout: UICollectionViewCompositionalLayout = {
+            let fraction: CGFloat = 1.0 / 3.0
+            
+            // Item
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            // Group
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(fraction), heightDimension: .fractionalWidth(fraction))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            // Section
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 100, leading: 2.5, bottom: 0, trailing: 2.5)
+            section.orthogonalScrollingBehavior = .continuous
+            section.visibleItemsInvalidationHandler = { (items, offset, environment) in
+                items.forEach { item in
+                    let distanceFromCenter = abs((item.frame.midX - offset.x) - environment.container.contentSize.width / 2.0)
+                    let minScale: CGFloat = 0.7
+                    let maxScale: CGFloat = 0.7
+                    let scale = max(maxScale - (distanceFromCenter / environment.container.contentSize.width), minScale)
+                    item.transform = CGAffineTransform(scaleX: scale, y: scale)
+                }
+            }
+            
+            return UICollectionViewCompositionalLayout(section: section)
+        }()
+        return compositionalLayout
+    }
+}
+
+private let reuseIdentifier = "Cell"
+
+class HomeController: UICollectionViewController {
+
     
-    
+    init() {
+        let compositionLayout = CompositionalLayout()
+        
+        //let layout = UICollectionViewFlowLayout()
+        let layout = compositionLayout.makeCompositionalLayoutViewControllerWithZoomingCarousel()
+        super.init(collectionViewLayout: layout)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        collectionView.backgroundColor = .white
-        navigationItem.titleView = titleView
-        navigationItem.largeTitleDisplayMode = . always
-        navigationItem.hidesBackButton = true
-        
-        
-        collectionView.register(EvenHeaderView, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: EventHeaderView.id)
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
     }
-}
+
+        
+        override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 10
+        }
+        
+        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+            cell.backgroundColor = .red
+            return cell
+        }
+    }
+   
